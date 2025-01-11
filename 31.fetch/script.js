2b4542ff286d6aa521db79767f7fbd74dba6e6a3
lisamaliqi <lisamaliqi@hotmail.com>
2025-01-11 15:27:11 +0100

    fetching data from dogs.json

--- 31.fetch/script.js
+++ 31.fetch/script.js
@@ -2,3 +2,17 @@
  * Fetch 🐶
  *
  */
+
+//använder fetch metoden för att hämta ut dogs
+fetch("data/dogs.json")
+	.then((res) => {
+        //om respons INTE är mellan 200-299 (aka success) så ska man skicka ett error
+		if (!res.ok) {
+			throw new Error("Request was not successful!");
+		}
+        //konvertera om json responsen till ett javascript objekt som är i en array 
+		return res.json();
+	})
+    //logga ut datan från dogs
+	.then(data => console.log("data:", data))
+	.catch(err => console.log("🚨 Something went wrong:", err));
