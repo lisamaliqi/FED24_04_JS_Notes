2126c34e4d1fe95ec30b457adf330f89c299f843
lisamaliqi <lisamaliqi@hotmail.com>
2025-01-11 15:50:52 +0100

    how to fetch dogs first and THEN fetch cats

--- 31.fetch/script.js
+++ 31.fetch/script.js
@@ -3,7 +3,7 @@
  *
  */
 
-//använder fetch metoden för att hämta ut dogs
+//använder fetch metoden för att hämta ut dogs och SEDAN hämta ut katter
 fetch("data/dogs.json")
 	.then((res) => {
         //om respons INTE är mellan 200-299 (aka success) så ska man skicka ett error
@@ -15,14 +15,35 @@ fetch("data/dogs.json")
 	})
     //logga ut datan från dogs
 	.then((data) => {
-        //logga ut datan samt skicka in data i DOM
+        //logga ut dogs samt skicka in det i DOM
         console.log("dogs:", data);
         document.querySelector('#dogs').innerHTML = data
             .map(dog => `<li>${dog.name}</li>`)
             .join('');
+
+        //retunera sedan att du fetchar cats
+        return fetch('data/cats.json');
+    })
+    .then((res) => {
+        if(!res.ok) {
+            throw new Error('Request was not successful!');
+        }
+
+        //retunera json filen som konverterad till ett javascript objekt 
+        return res.json();
+    })
+    .then((data) => {
+        //logga ut dogs samt skicka in det i DOM
+        console.log("cats:", data);
+        document.querySelector('#cats').innerHTML = data
+            .map(cat => `<li>${cat.name}</li>`)
+            .join('');
     })
 	.catch(err => console.log("🚨 Something went wrong:", err));
 
+
+/*
+//man fetchar katter samtidigt som man fetchar hundarna och katterna 
 // Gör samma för katter:
 fetch("data/cats.json")
 	.then((res) => {
@@ -38,5 +59,4 @@ fetch("data/cats.json")
 			.join("");
 	})
 	.catch(err => console.log("🚨 Something went wrong:", err));
-
-           
+*/ 
