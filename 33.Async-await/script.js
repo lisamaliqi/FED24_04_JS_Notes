95ba8da57579ef69d173ddd63615f7d65188ac84
lisamaliqi <lisamaliqi@hotmail.com>
2025-01-29 18:04:25 +0100

    adding comments to the code

--- 33.Async-await/script.js
+++ 33.Async-await/script.js
@@ -2,16 +2,24 @@
  * async/await 🐶
  *
  */
+
+//hämta data från filen data/cats
 fetch("data/cats.json")
 	.then(res => {
-		if (!res.ok) {
+		if (!res.ok) { //om respons inte är ok (aka något blivit fel med responsen) skicka error meddelande
 			throw new Error("Request was not successful!");
 		}
 		return res.json();
 	})
-	.then(data => {
+	.then(data => { 
+        //ifall allt är okej, skicka datan till consolen
 		console.log("data:", data);
 	})
 	.catch(err => {
+        //ifall den fångar ett error (typ fel namn på fil osv) skicka error
 		console.log("Nope, something went wrong:", err);
 	});
+
+
+
+    
