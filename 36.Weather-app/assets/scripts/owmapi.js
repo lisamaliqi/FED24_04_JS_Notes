5330c796a8aa6badd25a1048d56a9f5ffdd8950a
lisamaliqi <lisamaliqi@hotmail.com>
2025-01-29 20:52:48 +0100

    fetching data from api

--- 36.Weather-app/assets/scripts/owmapi.js
+++ 36.Weather-app/assets/scripts/owmapi.js
@@ -3,5 +3,29 @@
  *
  * <https://openweathermap.org/current>
  */
-const API_KEY = "";
+
+//hämta api-key som jag själv genererat i openweathermap 
+const API_KEY = "a2dcea2a4c82329c40bdaf0ef20c66f3";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+
+/**
+ * Hämta vad vädret är i en stad genom OpenWeatherMap API
+ *
+ * @param {string} city City to get current weather for
+ */
+const getCurrentWeather = async (city) => {
+    // Hämta vädret i 'city' (detta är vad användaren kommer skriva in sendare)
+	const response = await fetch(`${BASE_URL}/weather?q=${city}&units=metric&appid=${API_KEY}`);
+
+    // Kolla ifall responsen är ok
+	if (!response.ok) {
+		throw new Error(`${response.status} ${response.statusText}`);   // "404 Not Found"
+	}
+
+    // Konvertera responsens body från json
+	const data = await response.json();
+
+    // Retunera vädret
+	return data;
+};
