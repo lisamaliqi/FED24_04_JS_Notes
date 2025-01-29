75d58437972c3c250b9f2d8ea421f848755229f7
lisamaliqi <lisamaliqi@hotmail.com>
2025-01-29 19:53:20 +0100

    adding workshop instructions

--- 35.Chuck-Norris/script.js
+++ 35.Chuck-Norris/script.js
@@ -3,4 +3,13 @@
  *
  * <https://api.chucknorris.io/>
  * <https://api.chucknorris.io/jokes/random>
+ *  *
+ * STEG 1
+ * Hämta ett slumpmässigt faktum från ovan API så fort sidan laddar och visa det i paragrafen med id `joke`.
+ *
+ * STEG 2
+ * Lägg till en knapp som hämtar ett slumpmässigt faktum när man klickar på knappen. Det ska fortfarande hämtas ett slumpmässigt faktum när sidan laddas.
+ *
+ * STEG 3
+ * Lägg in felhantering för om hämtningen av ett slumpmässigt faktum misslyckas (av någon anledning). Visa felmeddelandet på sidan (i DOM).
  */
